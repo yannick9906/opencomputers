1e21a7a88a78e6e1768ffc76f92c505a05d2056a
Yannick Felix <yannick.felix1999@gmail.com>
2016-03-04 13:25:51 +0100

    turbines

--- files/turbines.lua
+++ files/turbines.lua
@@ -7,6 +7,7 @@
 --
 
 local pid = require("pid")
+local shell = require("shell")
 local component = require("component")
 
 turbines = component.list("turbine")
@@ -14,5 +15,6 @@ turbines = component.list("turbine")
 print("Looking for Turbines on the network...")
 for address, componentType in turbines do
     print("Found at: <<"..address..">> Type: "..componentType)
+    shell.execute("pid run /pids/turbine.pid p=10 i=0.4 d=1 f=4 target=1820 --args "..address)
 end
 print("Finished")
