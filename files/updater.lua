00ff2020a9e0163ed063d38fc70c0ee03ab0b472
Yannick Felix <yannick.felix1999@gmail.com>
2016-04-03 19:35:55 +0200

    updater update

--- files/updater.lua
+++ files/updater.lua
@@ -9,6 +9,8 @@ local shell = require("shell")
 
 shell.execute("rm turbines.lua")
 shell.execute("rm updater.lua")
+shell.execute("rm redcontrol.lua")
 shell.execute("wget https://bitbucket.org/yannick9906/opencomputers/raw/master/files/turbines.lua")
 shell.execute("wget https://bitbucket.org/yannick9906/opencomputers/raw/master/files/updater.lua")
+shell.execute("wget https://bitbucket.org/yannick9906/opencomputers/raw/master/files/redcontrol.lua")
 
