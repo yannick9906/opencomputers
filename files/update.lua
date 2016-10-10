08d89d888dba0e3dc04235be25a46045ce6fa7c0
Yannick Felix <yannick.felix1999@gmail.com>
2016-10-10 23:21:02 +0200

    Redcontrol

---
+++ files/update.lua
@@ -0,0 +1,10 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: yanni
+-- Date: 10.10.2016
+-- Time: 23:20
+-- To change this template use File | Settings | File Templates.
+--
+local shell = require("shell")
+
+shell.execute("wget -f https://raw.githubusercontent.com/yannick9906/opencomputers/master/files/server.lua")
