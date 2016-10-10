e32dd03613f8d8c72455b4fd9d08d76e063b3c0a
Yannick Felix <yannick.felix1999@gmail.com>
2016-10-10 23:25:48 +0200

    Redcontrol

--- files/server.lua
+++ files/server.lua
@@ -6,5 +6,13 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-
-
+local component = require("component")
+local event = require("event")
+local m = component.modem -- get primary modem component
+m.open(123)
+print(m.isOpen(123)) -- true
+-- Send some message.
+m.broadcast(321, "this is a test")
+-- Wait for a message from another network card.
+local _, _, from, port, _, message = event.pull("modem_message")
+print("Got a message from " .. from .. " on port " .. port .. ": " .. tostring(message))
